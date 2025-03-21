b7f8d8cfaf48a366625fa99b58924bc6822da2b4
JeanChipo <max.lef@free.fr>
2025-03-21 11:52:39 +0100

    Ajout des images de test Base de main Ajout du header de bmp8

---
+++ bmp8.h
@@ -0,0 +1,20 @@
+#ifndef BMP8
+#define BMP8
+
+typedef struct {
+    unsigned char header[54];
+    unsigned char colorTable[1024];
+    unsigned char * data;
+
+    unsigned int width; // offset à 18 de l'en-tête  
+    unsigned int height; // offset à 22 de l'en-tête
+    unsigned int colorDepth; // offset à 28 de l'en-tête
+    unsigned int dataSize; // offset à 34 de l'en-tête
+} t_bmp8;
+
+t_bmp8 * bmp8_loadImage(const char * filename);
+void bmp8_saveImage(const char * filename, t_bmp8 * img);
+void bmp8_free(t_bmp8 * img);
+void bmp8_printInfo(t_bmp8 * img);
+
+#endif // BMP8
