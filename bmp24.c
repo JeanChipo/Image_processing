09cb1d965f0a0640d07951e007ed103d70cb858e
JeanChipo <max.lef@free.fr>
2025-05-19 10:56:46 +0200

    Added bmp24

---
+++ bmp24.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "bmp24.h"
+
+
+t_pixel ** bmp24_allocateDataPixels (int width, int height) {
+// Allouer dynamiquement de la mémoire pour une matrice de t_pixel de taille
+// witdh × height, et renvoyer l’adresse allouée dans le tas. Si l’allocation échoue, la fonction affiche un
+// message d’erreur et renvoie NULL.
+}
