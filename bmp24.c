722ed2f4c6077790d3871cd45b2f57fb25bfef08
JeanChipo <max.lef@free.fr>
2025-05-22 11:46:05 +0200

    Fix main.c

--- bmp24.c
+++ bmp24.c
@@ -5,7 +5,7 @@
 
 t_bmp24 * bmp24_loadImage(const char * filename) {
     FILE *file = fopen(filename, "rb"); // rb for read binary
-    int img;
+    t_bmp24 * img;
     
     t_bmp_header *header = (t_bmp_header *)malloc(sizeof(t_bmp_header)); // allocate memory for the header
     header->type = fread(header, sizeof(unsigned char), 16, file);
@@ -26,7 +26,6 @@ t_bmp24 * bmp24_loadImage(const char * filename) {
     info->yresolution = fread(info, sizeof(unsigned char), 32, file);
     info->ncolors = fread(info, sizeof(unsigned char), 32, file);
     info->importantcolors = fread(info, sizeof(unsigned char), 32, file);
-    
         
     img = bmp24_allocate (header, info, 24);
 
@@ -41,34 +40,33 @@ void bmp24_saveImage(const char * filename, t_bmp24 * img) {
 
     // Écriture du header BMP (14 octets)
     t_bmp_header *header = &img->header;
-    fwrite(header->type, sizeof(uint16_t), 16, file);
-    fwrite(header->size, sizeof(uint32_t), 32, file);
-    fwrite(header->reserved1, sizeof(uint16_t), 16, file);
-    fwrite(header->reserved2, sizeof(uint16_t), 16, file);
-    fwrite(header->offset, sizeof(uint32_t), 32, file);
+    fwrite(&header->type, sizeof(uint16_t), 16, file);
+    fwrite(&header->size, sizeof(uint32_t), 32, file);
+    fwrite(&header->reserved1, sizeof(uint16_t), 16, file);
+    fwrite(&header->reserved2, sizeof(uint16_t), 16, file);
+    fwrite(&header->offset, sizeof(uint32_t), 32, file);
     free(header);
 
     // Écriture du header info BMP (généralement 40 octets)
     t_bmp_info *header_info = &img->header_info;
-    fwrite(header_info->size, sizeof(uint32_t), 32, file);
-    fwrite(header_info->width, sizeof(int32_t), 32, file);
-    fwrite(header_info->height, sizeof(int32_t), 32, file);
-    fwrite(header_info->planes, sizeof(uint16_t), 16, file);
-    fwrite(header_info->bits, sizeof(uint16_t), 16, file);
-    fwrite(header_info->compression, sizeof(uint32_t), 32, file);
-    fwrite(header_info->imagesize, sizeof(uint32_t), 32, file);
-    fwrite(header_info->xresolution, sizeof(int32_t), 32, file);
-    fwrite(header_info->yresolution, sizeof(int32_t), 32, file);
-    fwrite(header_info->ncolors, sizeof(uint32_t), 32, file);
-    fwrite(header_info->importantcolors, sizeof(uint32_t), 32, file);
-    free(header_info);
+    fwrite(&header_info->size, sizeof(uint32_t), 32, file);
+    fwrite(&header_info->width, sizeof(int32_t), 32, file);
+    fwrite(&header_info->height, sizeof(int32_t), 32, file);
+    fwrite(&header_info->planes, sizeof(uint16_t), 16, file);
+    fwrite(&header_info->bits, sizeof(uint16_t), 16, file);
+    fwrite(&header_info->compression, sizeof(uint32_t), 32, file);
+    fwrite(&header_info->imagesize, sizeof(uint32_t), 32, file);
+    fwrite(&header_info->xresolution, sizeof(int32_t), 32, file);
+    fwrite(&header_info->yresolution, sizeof(int32_t), 32, file);
+    fwrite(&header_info->ncolors, sizeof(uint32_t), 32, file);
+    fwrite(&header_info->importantcolors, sizeof(uint32_t), 32, file);
 
     // Écriture des pixels (BGR, ligne par ligne, avec padding)
     for (int i = 0; i < img->header_info.width*img->header_info.height; i++) {
         t_pixel *pixel = img->data[i];
-        fwrite(pixel->red, sizeof(uint8_t), 8, file);
-        fwrite(pixel->green, sizeof(uint8_t), 8, file);
-        fwrite(pixel->blue, sizeof(uint8_t), 8, file);
+        fwrite(&pixel->red, sizeof(uint8_t), 8, file);
+        fwrite(&pixel->green, sizeof(uint8_t), 8, file);
+        fwrite(&pixel->blue, sizeof(uint8_t), 8, file);
     }
 
     fclose(file);
